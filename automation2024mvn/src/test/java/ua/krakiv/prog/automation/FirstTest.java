959e2ceb533b97dd0cf74ff4d65d9a67be703fab
Daria Tretiakova <daria.tretiakova@raiffeisen.ua>
2024-11-10 21:13:27 +0200

    changes in dependencies

---
+++ automation2024mvn/src/test/java/ua/krakiv/prog/automation/FirstTest.java
@@ -0,0 +1,11 @@
+package ua.krakiv.prog.automation;
+
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+    @Test
+    public void testMethod(){
+        System.out.println("Hello world!");
+    }
+}
